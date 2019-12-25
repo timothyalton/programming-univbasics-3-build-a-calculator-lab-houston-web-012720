aba5c0b25d0dc3da006fe8b1278a8b6013f1c25f
Mitchell T Alton <mitchell.alton@live.com>
2019-12-25 21:25:53 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+p 5 + 4
 end
 
 def subtraction(num1, num2)
+  p 10 - 5 
 
 end
 
 def division(num1, num2)
+  p 50 / 2 
 
 end
 
 def multiplication(num1, num2)
+  p 4 * 30 
 
 end
 
 def modulo(num1, num2)
+  p 34 % 5 
 
 end
 
 def square_root(num)
+  p Math.sqrt(81)
 
 end
